ddd4025f9dd72c80b001989e04a1d9b0e3343d55
Kristian Sakarisson <kristian@sakarisson.com>
2017-10-09 22:00:57 +0100

    switched to shared pointer

--- Trains/Train.cpp
+++ Trains/Train.cpp
@@ -17,16 +17,16 @@ Train::~Train() {
 bool Train::addCar(CarType type) {
     switch (type) {
     case COACHCAR:
-        _cars.push_back(make_unique<CoachCar>());
+        _cars.push_back(make_shared<CoachCar>());
         break;
     case SLEEPINGCAR:
-        _cars.push_back(make_unique<SleepingCar>());
+        _cars.push_back(make_shared<SleepingCar>());
         break;
     case OPENFREIGHTCAR:
-        _cars.push_back(make_unique<OpenFreightCar>());
+        _cars.push_back(make_shared<OpenFreightCar>());
         break;
     case COVEREDFREIGHTCAR:
-        _cars.push_back(make_unique<CoveredFreightCar>());
+        _cars.push_back(make_shared<CoveredFreightCar>());
         break;
     default:
         return false;
