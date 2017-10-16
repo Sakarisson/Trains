87a4d6dea123df6badd5cbb217d4c02f81cabc00
Kristian Sakarisson <kristian@sakarisson.com>
2017-10-16 15:53:20 +0100

    Added Time class

---
+++ Trains/Time.cpp
@@ -0,0 +1,27 @@
+#include "Time.h"
+#include <iomanip>
+#include <sstream>
+
+
+Time::Time(std::string stringFormattedTime) {
+    int hours = stoi(stringFormattedTime.substr(0, stringFormattedTime.find(":")));
+    int minutes = stoi(stringFormattedTime.substr(stringFormattedTime.find(":") + 1));
+    _minutesSinceMidnight = hours * 60 + minutes;
+}
+
+
+Time::~Time() {
+    
+}
+
+int Time::getMinutes() const {
+    return _minutesSinceMidnight;
+}
+
+std::string Time::getString() const {
+    int hours = _minutesSinceMidnight / 60;
+    int minutes = _minutesSinceMidnight - (hours * 60);
+    std::stringstream ss;
+    ss << std::setw(2) << std::setfill('0') << hours << ":" << std::setw(2) << std::setfill('0') << minutes;
+    return ss.str();
+}
---
+++ Trains/Time.h
@@ -0,0 +1,18 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <string>
+
+class Time
+{
+private:
+    int _minutesSinceMidnight;
+public:
+    Time(int mins) : _minutesSinceMidnight(mins) {}
+    Time(std::string);
+    ~Time();
+    int getMinutes() const;
+    std::string getString() const;
+};
+
+#endif // !TIME_H
